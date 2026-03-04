de4dd2d457642d5638284622071cbc6b05339608
nicolasmm34 <nicolasmm34@educastur.es>
2026-03-04 19:40:40 +0100

    Creacion de la clase App

--- src/codigo/App.java
+++ src/codigo/App.java
@@ -3,8 +3,7 @@ package codigo;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Este archivo lanzará la aplicación");
 	}
 
 }
