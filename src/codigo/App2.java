93f6077bc06c2a6820304816f66e9e399891d0b0
nicolasmm34 <nicolasmm34@educastur.es>
2026-03-04 19:42:23 +0100

    Edicion de App y nombre

--- src/codigo/App2.java
+++ src/codigo/App2.java
@@ -1,9 +1,10 @@
 package codigo;
 
-public class App {
+public class App2 {
 
 	public static void main(String[] args) {
 		System.out.println("Este archivo lanzará la aplicación");
+		System.out.println("Nicolás");
 	}
 
 }
